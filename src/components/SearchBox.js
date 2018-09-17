f720a49a8cc2c1b428b710e2b42db951cebaa4a6
ptsavdar <ptsavdar@gmail.com>
2018-09-17 22:16:10 +0200

    Optimizations

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ import React from 'react';
 const SearchBox = ({searchChange}) =>
   <div className={'pa2'}>
     <input
+      aria-label="Search Robots"
       className={'pa3 ba b--green bg-lightest-blue'}
       type={'search'}
       placeholder={'search robots'}
